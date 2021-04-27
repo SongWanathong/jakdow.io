8bb33a98d97afed418b88c0a16f7c72cc05b0cf2
SongWanathong <58030142@kmitl.ac.th>
2021-04-27 16:52:59 +0700

    52c7d25

---
+++ _nuxt/static/1619517125/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"jakdow.io\u002F_nuxt\u002Fstatic\u002F1619517125",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"jakdow.io\u002F",assetsPath:"jakdow.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
